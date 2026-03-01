8cb20650343959d2f192cdaa65d1896565f5fcd4
ehall-4 <148909455+ehall-4@users.noreply.github.com>
2026-03-01 09:18:23 -0500

    Small additions

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import subprocess
 import discord
 from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
 
+
+# ------------------------------------------------
+# Load environment variables and my user ID
+# ------------------------------------------------
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+
+OWNER_ID = 127634987854987264
+
+# ------------------------------------------------
+# Initialize bot, logging and intents
+# ------------------------------------------------
 
 handler = logging.FileHandler(filename="discord.log", encoding='utf-8', mode='w')
 intents = discord.Intents.default()
@@ -13,6 +24,10 @@ intents.message_content= True
 intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+# ------------------------------------------------
+# Events
+# ------------------------------------------------
 
 @bot.event
 async def on_ready():
@@ -23,13 +38,39 @@ async def on_message(message):
     if message.author == bot.user:
         return
     if "frog" in message.content.lower():
-        await message.channel.send(f"{message.author.mention}, what ARE frogs?")
+        await message.reply(f"What *are* frogs? :frog:")
         
     await bot.process_commands(message)
 
+# ------------------------------------------------
+# '!' Commands
+# ------------------------------------------------
+
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f"Hello {ctx.author.mention}!")
-
+    await ctx.reply(f"Hello {ctx.author.mention}!")
+    
+@bot.command()
+async def startserver(ctx):
+    await ctx.send("Starting Minecraft server...")
+    
+    subprocess.Popen([
+        "sudo", "-u", "minecraft",
+        "/opt/server/start.sh"
+        ])
+    
+    await ctx.send("Minecraft server is running!")
+   
+@bot.command()
+async def shutdown(ctx):
+    if ctx.author.id != OWNER_ID:
+        await ctx.send("You do not have permission to use this command.")
+        return
+    
+    await ctx.send("Shutting down...")
+    
+    subprocess.Popen([
+        "sudo", "/bin/systemctl", "stop", "discordbot"
+    ])
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
